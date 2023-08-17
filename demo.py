8f4f393ba9a17463180f50e263ab04bc7b2313c4
lalith1021 <lalith1021@gmail.com>
2023-08-17 13:10:58 +0530

    search sort

--- demo.py
+++ demo.py
@@ -1,1 +1,15 @@
 print("Hi from git")
+#selection sort
+def sort(a):
+    for i in range(0,len(a)-1):
+        min=i
+        for j in range(i,len(a)):
+            if a[j]<a[min]:
+                min=j
+        a[i],a[min]=a[min],a[i]
+        print(a)
+    print("Final sorted:")
+    print(a)
+
+a=[800,100,130,30,0]
+sort(a)
